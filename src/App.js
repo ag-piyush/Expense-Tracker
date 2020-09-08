bfda226677910807667a43e06404a58b9b4761a3
Piyush Agarwal <piyush.ag.bitsg@gmail.com>
2020-09-08 14:55:54 +0530

    Transaction List

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Header } from './components/Header'
-import { Balance } from './components/Balance'
-import { IncomeExpenses } from './components/IncomeExpenses'
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className = "container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
