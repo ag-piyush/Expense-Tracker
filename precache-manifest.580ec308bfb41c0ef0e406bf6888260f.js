6c85ff73fc9ab60479f3c36b5039b6705e4bd7a6
Piyush Agarwal <piyush.ag.bitsg@gmail.com>
2020-10-22 22:44:16 +0530

    Updates

---
+++ precache-manifest.580ec308bfb41c0ef0e406bf6888260f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be259b915bbb105aef7bf03ff1cec6cd",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "faaee6efd182f712c494",
+    "url": "/Expense-Tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "2929d7d1b5989251056f",
+    "url": "/Expense-Tracker/static/js/2.ea9e118a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker/static/js/2.ea9e118a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faaee6efd182f712c494",
+    "url": "/Expense-Tracker/static/js/main.6c26e0c1.chunk.js"
+  },
+  {
+    "revision": "d8a91e55adfc133c33b9",
+    "url": "/Expense-Tracker/static/js/runtime-main.89d3d3ed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/expense-tracker/precache-manifest.66835a9b16443d60ece6785ca72979fa.js"
+  "/Expense-Tracker/precache-manifest.580ec308bfb41c0ef0e406bf6888260f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/expense-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Expense-Tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
