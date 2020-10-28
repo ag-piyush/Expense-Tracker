8e1e2a96b7becb9c46d8ef92236113bb0f14489a
Piyush Agarwal <piyush.ag.bitsg@gmail.com>
2020-10-29 04:37:03 +0530

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer>
+            <p className="copyright">© 2020, Made with &hearts; <a href="https://github.com/ag-piyush">Piyush Agarwal</a></p> 
+        </footer>
+    );
+}
+
+export default Footer;
